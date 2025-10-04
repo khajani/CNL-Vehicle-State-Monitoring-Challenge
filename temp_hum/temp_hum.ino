e4df2e099ab0860109ecb279bc4b17812ac9f360
adhamaibrahim <adham.ah.ibrahim@gmail.com>
2025-10-04 16:39:54 -0400

    Update temp_hum.ino

--- temp_hum/temp_hum.ino
+++ temp_hum/temp_hum.ino
@@ -1,5 +1,37 @@
 #include "Arduino_SensorKit.h"
 #include <rgb_lcd.h>
+#include <Wire.h>
+#include <AHT20.h>
+AHT20 aht20;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("AHT20 example");
+
+  Wire.begin();
+  if (aht20.begin() == false)
+  {
+    Serial.println("AHT20 not detected. Please check wiring. Freezing.");
+    while(true);
+  }
+}
+
+void loop()
+{
+  float temperature = aht20.getTemperature();
+  float humidity = aht20.getHumidity();
+
+  Serial.print("T: ");
+  Serial.print(temperature, 2);
+  Serial.print(" C\t H: ");
+  Serial.print(humidity, 2);
+  Serial.println("% RH");
+
+  delay(2000);
+}
+
+
 
 rgb_lcd lcd;
 
@@ -12,21 +44,20 @@ void setup() {
   // Display.backlight();
 
   // Start the temperature/humidity sensor
-  Environment.setPin(6);  
-  Environment.begin();
+  //Environment.begin();
 }
 
-void loop() {
-  float temperature = Environment.readTemperature();
-  float humidity = Environment.readHumidity();
+//void loop() {
+  //float temperature = Environment.readTemperature();
+  //float humidity = Environment.readHumidity();
 
-  Serial.print("Temperature = ");
-  Serial.print(Environment.readTemperature());
-  Serial.println(" C");
+  //Serial.print("Temperature = ");
+  //Serial.print(Environment.readTemperature());
+  //Serial.println(" C");
 
-  Serial.print("Humidity = ");
-  Serial.print(Environment.readHumidity());
-  Serial.println(" %");
+  //Serial.print("Humidity = ");
+  //Serial.print(Environment.readHumidity());
+  //Serial.println(" %");
 
   // Clear LCD and display message if temp > 25
   // Display.clear();
